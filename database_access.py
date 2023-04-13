b6fd56d2a602cf92cd1254df1acad282bed0c266
shmjade <shm.jade@gmail.com>
2023-04-13 11:34:06 -0300

    add codificarConsultas function

--- database_access.py
+++ database_access.py
@@ -5,31 +5,6 @@ from sqlalchemy import create_engine
 #Create engine: engine
 engine = create_engine("postgresql://postgres:giu@127.0.0.1:5432/postgres")
 
-def menu():
-    print("==> Menu <==")
-    print("Selecione um dos tipos de consulta disponíveis: ")
-    print("1) Nome e quantidade de reservas de cada país")
-    print("2) Nome dos usuários que já gastaram mais do que a média global")
-    print("3) Nomes de cidades que só tem acomodações com preços menores que a média de preços global")
-    print("4) Acomodações em determinada localização ordenadas por preço")
-    print("5) Consultar para quais cidades e em qual data um Guest já viajou")
-    print("6) Nomes dos países que todas as cidades têm pelo menos uma acomodação")
-    print("7) Quantidade de vezes que um Guest foi para um país")
-    print("8) Filtrar por número de quartos e cidade")
-    print("9) Acomodações dos usuários agrupados por lista")
-    print("10) Acomodações de um host que já tiveram reserva solicitada e sua cidade")
-    print("===== Pressione S para sair do programa ======")
-    print("Opção: ", end="")
-
-    opcao = int(input())
-    while(opcao<1 or opcao>10):
-        print("Opção inválida, digitar um valor entre 0 e 10")
-        print("Opção: ", end="")
-        opcao = int(input())
-    return opcao
-
-menu()
-
 # 4: localizacao, 5:guest name, 7: guest name, 8: numero de quartos e nome da cidade, 10: nome do host
 def menu():
     print("==> Menu <==")
@@ -44,19 +19,19 @@ def menu():
     print("8) Filtrar por número de quartos e cidade")
     print("9) Acomodações dos usuários agrupados por lista")
     print("10) Acomodações de um host que já tiveram reserva solicitada e sua cidade")
-    print("===== Pressione S para sair do programa ======")
+    print("===== Pressione 0 para sair do programa ======")
     print("Opção: ", end="")
 
 
     opcao = int(input())
-    while(opcao.upper() !='S' and opcao<1 or opcao>10):
+    while(opcao<0 or opcao>10):
         print("Opção inválida, digitar um valor entre 0 e 10")
         print("Opção: ", end="")
         opcao = int(input())
     return opcao
 
 
-def chamarConsultas(opcao):
+def codificarConsultas(opcao):
     match opcao:
         case 1:
             query = "SELECT Country_name, count(Reservation_id) as qtReservations FROM AccommodationWithLocation A JOIN Reservations R ON A.Accommodation_id=R.fk_Accommodation_Id WHERE Was_approved = 'true' GROUP BY Country_name;"
@@ -91,6 +66,17 @@ def chamarConsultas(opcao):
             host = input()
             query = "SELECT Accommodation_id, City_name FROM AccommodationWithLocation A JOIN Users U ON A.fk_User_Id=U.User_Id WHERE User_name = " + host + " AND Accommodation_id IN (SELECT Accommodation_id FROM Reservations);"
     return query
+
+
+
+############### MAIN ##############
+opcao = menu()
+while(opcao!=0):
+    pandas.read_sql(codificarConsultas(opcao), engine)
+    opcao = menu()
+print("Você pressionou 0. Programa encerrado.")
+
+
 '''
     consulta1 = pandas.read_sql("SELECT Country_name, count(Reservation_id) as qtReservations FROM AccommodationWithLocation A JOIN Reservations R ON A.Accommodation_id=R.fk_Accommodation_Id WHERE Was_approved = 'true' GROUP BY Country_name;", engine)
     consulta2 = pandas.read_sql("SELECT User_name FROM AccommodationWithLocation A JOIN UsersReservations U ON A.Accommodation_id=U.fk_Accommodation_Id GROUP BY User_id, User_name HAVING SUM(Final_price)>(SELECT AVG(Final_price) FROM AccommodationWithLocation A JOIN UsersReservations U ON A.Accommodation_id=U.fk_Accommodation_Id);", engine)
